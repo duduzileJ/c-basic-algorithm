5bf23fbb451c7324f4600f8fd296d25a1e106eca
DuduJ <98821469+duduzileJ@users.noreply.github.com>
2022-03-22 11:39:01 +0000

    added code that     //gets the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ class Program {
   public static void Main (string[] args) {
     //https://www.w3resource.com/csharp-exercises/basic-algo/index.php
     //computes the sum of the two given integer values. If the two values are the same, then return triple their sum
-    Console.WriteLine("Triple the sum");
+    Console.WriteLine("Triple the sum if numbers are the same");
     Console.Write("Enter first number: ");
     int num1 = int.Parse(Console.ReadLine());
     Console.Write("Enter second number: ");
@@ -19,6 +19,20 @@ class Program {
     {
       sum = num1 + num2;
       Console.WriteLine(sum);
+    }
+
+    //get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.
+    Console.WriteLine("\nGet the absolute difference between entered number and 51");
+    Console.Write("Enter number: ");
+    int n = int.Parse(Console.ReadLine());
+    int difference = n - 51;
+    if(n > 51)
+    {
+      difference= difference *3;
+      Console.WriteLine(difference);
+    }
+    else{
+      Console.WriteLine(difference);
     }
     
     
